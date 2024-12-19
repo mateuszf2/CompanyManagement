c2a5c1022be8fbe7d44e18d536843ff79e516e2b
mateuszf2 <mateusz.fabich@wp.pl>
2024-12-19 09:20:15 +0100

    Backend dla projektów

--- server.js
+++ server.js
@@ -84,13 +84,17 @@ app.get(personEndpoint, auth.checkIfInRole([0, 1]), (req, res) => {
 
     const aggregation = [ matching ]
 
-    aggregation.push({ $match: { firstName: { $regex: req.query.firstName || '' }}})
-    aggregation.push({ $match: { lastName: { $regex: req.query.lastName || '' }}})
     if(req.query.sort) {
         aggregation.push({ $sort: sort })
     }
-    aggregation.push({ $skip: +req.query.skip || 0 })
-    aggregation.push({ $limit: +req.query.limit || 1000000 })
+    const skip = +req.query.skip || 0
+    if(skip > 0) {
+        aggregation.push({ $skip: skip })
+    }
+    const limit = +req.query.limit || 0
+    if(limit > 0) {
+        aggregation.push({ $limit: limit })
+    }
     Person.aggregate([{ $facet: {
         total: [ matching, { $count: 'count' } ],
         data: aggregation
@@ -152,6 +156,114 @@ app.delete(personEndpoint, auth.checkIfInRole([0]), (req, res) => {
     }
 })
 
+const projectSchema = new mongoose.Schema({
+    _id: { type: String, default: uuid.v4 },
+    name: { type: String, required: true, validate: {
+        validator: value => /^\p{L}/u.test(value),
+        message: props => `${props.value} nie pasuje do wymagań`
+    }},
+    startDate: { type: Date, required: true, validate: {
+        validator: value => !!(new Date()),
+        message: props => `${props.value} nie jest prawidłową datą urodzenia`
+        }, transform: value => value.toISOString().substr(0, 10)
+    },
+    endDate: { type: Date, required: false, validate: {
+        validator: value => !!(new Date()),
+        message: props => `${props.value} nie jest prawidłową datą urodzenia`
+        }, transform: value => value.toISOString().substr(0, 10)
+    }
+}, {
+    versionKey: false,
+    additionalProperties: false
+})
+
+const projectEndpoint = '/api/project'
+let Project = null
+
+app.get(projectEndpoint, auth.checkIfInRole([0, 1]), (req, res) => {
+    // pobierz wszystkie rekordy z bazy do zmiennej project
+    let sort = {}
+    if(req.query.sort) {
+        sort[req.query.sort] = +req.query.order || 1
+    }
+    const matching = {
+        $match: { name: { $regex: req.query.search || '' }}
+    }
+
+    const aggregation = [ matching ]
+
+    if(req.query.sort) {
+        aggregation.push({ $sort: sort })
+    }
+    const skip = +req.query.skip || 0
+    if(skip > 0) {
+        aggregation.push({ $skip: skip })
+    }
+    const limit = +req.query.limit || 0
+    if(limit > 0) {
+        aggregation.push({ $limit: limit })
+    }
+    Project.aggregate([{ $facet: {
+        total: [ matching, { $count: 'count' } ],
+        data: aggregation
+    }}])
+    .then(facet => {
+        [ facet ] = facet
+        facet.total = ( facet.total && facet.total[0] ? facet.total[0].count : 0) || 0
+        facet.data = facet.data.map(project => new Project(project))
+        res.json(facet)
+    })
+    .catch(err => {
+        res.status(400).json({ error: 'Nieudany odczyt z bazy' })
+    })    
+})
+
+app.post(projectEndpoint, auth.checkIfInRole([0]), (req, res) => {
+    if(req.body) {
+        // utwórz obiekt na podstawie req.body, zwaliduj go i zapisz do bazy
+        const project = new Project(req.body)
+        const err = project.validateSync()
+        if(err) {
+            res.status(400).json({ error: err.message })
+            return
+        }
+        project.save()
+        .then(projectAdded => {
+            res.json(projectAdded)
+        })
+        .catch(err => res.status(400).json({ error: err.message, set: false }))
+    } else {
+        res.status(400).json({ error: 'Brak danych', set: false })
+    }
+})
+
+app.put(projectEndpoint, auth.checkIfInRole([0]), (req, res) => {
+    if(req.body && req.body._id) {
+        const _id = req.body._id
+        delete req.body._id
+        Project.findOneAndUpdate({ _id }, { $set: req.body }, { new: true, runValidators: true })
+        .then(projectUpdated => {
+            res.json(projectUpdated)
+        })
+        .catch(err => res.status(400).json({ error: err.message }))
+    } else {
+        res.status(400).json({ error: 'Brak danych do aktualizacji' })
+    }
+})
+
+app.delete(projectEndpoint, auth.checkIfInRole([0]), (req, res) => {
+    if(req.query._id) {
+        const _id = req.query._id
+        Project.findOneAndDelete({ _id })
+        .then(projectDeleted => {
+            res.json(projectDeleted)
+        })
+        .catch(err => res.status(400).json({ error: err.message }))
+    } else {
+        res.status(400).json({ error: 'Brak danych do usunięcia' })
+    }
+})
+
 try {
     config = JSON.parse(fs.readFileSync('config.json'))
     console.log('Konfiguracja z config.json')
@@ -163,6 +275,7 @@ mongoose.connect(config.dbUrl)
 .then(conn => {
     console.log('Połączenie z bazą danych nawiązane')
     Person = conn.model('person', personSchema)
+    Project = conn.model('project', projectSchema)
 })
 .catch(err => {
     console.error('Połączenie z bazą danych nieudane', err)
